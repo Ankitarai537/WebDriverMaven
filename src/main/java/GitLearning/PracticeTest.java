e558d2e54a8f0cf02c1071657b97f3a50ee17c65
Ankita <ankitarai537@gmail.com>
2021-01-07 01:50:47 +0530

    1st

---
+++ src/main/java/GitLearning/PracticeTest.java
@@ -0,0 +1,32 @@
+package GitLearning;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class PracticeTest {
+	
+	
+		public static WebDriver driver;
+		
+		
+		@BeforeMethod
+		public void loginStart() {
+			
+		System.setProperty("webdriver.ie.driver", "C:\\Users\\ANKITA\\Desktop\\OOPS\\GitWebdriverMaven\\src\\test\\resources\\executables\\IEDriverServer.exe");
+		 driver=new InternetExplorerDriver();
+		driver.get("https://www.google.com");}
+	
+		
+		@Test
+		public void login() throws Throwable
+		{
+			System.out.println("hiiii");
+			Thread.sleep(8000);
+			driver.findElement(By.xpath("//input[@title='Search']")).sendKeys("Amazon");
+		}
+	}
+
+
